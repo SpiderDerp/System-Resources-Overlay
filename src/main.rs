c938a0a90be381dbfe348f5d0756924335400deb
SpiderDerp <62308557+SpiderDerp@users.noreply.github.com>
2022-05-16 09:22:09 -0400

    works with windows now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-
-use fltk::{app, prelude::*, window::*, enums::{Color, Align}, image::*, frame::*, text::*, widget::*, group::*};
+use fltk::{
+    app,
+    enums::{Align, Color},
+    frame::*,
+    group::*,
+    image::*,
+    prelude::*,
+    text::*,
+    widget::*,
+    window::*,
+};
 use sys_info;
 
 fn main() {
@@ -21,16 +30,16 @@ fn main() {
     let ram = sys_info::mem_info().unwrap();
     let ram_usage_percent = (ram.free as f64 / ram.total as f64) * 100.0;
 
-    let mut _image = JpegImage::load(&std::path::Path::new("images/quandaledingle.jpg")).unwrap();
+    //let mut _image = JpegImage::load(&std::path::Path::new("images/quandaledingle.jpg")).unwrap();
     let mut fra = Frame::new(0, 0, 200, 50, "Ram Usage:");
 
-    let mut text = TextDisplay::new(0,0,200,100, "");
+    let mut text = TextDisplay::new(0, 0, 200, 100, "");
     text.set_label(format!("{:.2}%", ram_usage_percent).as_str());
 
     pack.add(&fra);
     pack.add(&text);
-    
-    while app.wait(){
+
+    while app.wait() {
         let ram = sys_info::mem_info().unwrap();
         let ram_usage_percent = (ram.free as f64 / ram.total as f64) * 100.0;
         text.set_label(format!("{:.2}%", ram_usage_percent).as_str());
